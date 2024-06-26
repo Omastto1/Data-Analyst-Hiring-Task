20d55a6d0ab96aad17aa48f867dd0135188af211
Tomas Omasta <omastto1@fel.cvut.cz>
2024-06-26 08:49:29 +0200

    add template main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+def main():
+    pass
+
+    # Load heinemann_de and aelia_uk products
+
+
+    # Concatenate data
+
+
+    # Load products mapping
+
+
+    # Keep only products available in products mapping (based on internal id and retailer)
+
+
+    # Split updated at into date and time
+
+
+    # Compute mean price for each product and day and assign it to "Mean price column"
+
+
+    # Keep only needed columns (title, internalid, retailer, country, actual_price, in_sale, url, price mean)
+
+
+    # Save resulting dataset into output.csv
+
+
+    # Generate dataset containing only last record for each product (not all products have been available on 2024-06-25)
+
+
+    # Save resulting dataaset into output_latest.csv
+
+
+if __name__ == '__main__':
+    main()
